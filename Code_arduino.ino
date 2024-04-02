50f828b47224010e07ddc89d22855c2d31a20d05
Matt <147386363+zarakiro@users.noreply.github.com>
2024-04-02 20:08:58 +0200

    Update Code_arduino.ino

--- Code_arduino.ino
+++ Code_arduino.ino
@@ -11,7 +11,7 @@
    value in EEPROM, and also how to change the value manually.
    The result value can then later be included in your project sketch or fetched from EEPROM.
 
-   To implement calibration in your project sketch the simplified procedure is as follow:
+   To implement calibration in your project sketch, follow the simplified procedure as follows:
        LoadCell.tare();
        //place known mass
        LoadCell.refreshDataSet();
@@ -23,167 +23,123 @@
 #include <EEPROM.h>
 #endif
 
-//pins:
-const int HX711_dout = 4; //mcu > HX711 dout pin
-const int HX711_sck = 5; //mcu > HX711 sck pin
+// Pins:
+const int HX711_dout = 4; // MCU > HX711 dout pin
+const int HX711_sck = 5; // MCU > HX711 sck pin
 
-//HX711 constructor:
+// HX711 constructor:
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 
-const int calVal_eepromAdress = 0;
+const int calVal_eepromAddress = 0;
 unsigned long t = 0;
 
 void setup() {
-  Serial.begin(57600); delay(10);
+  Serial.begin(57600);
+  delay(10);
   Serial.println();
   Serial.println("Starting...");
 
   LoadCell.begin();
-  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
-  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
-  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
-  LoadCell.start(stabilizingtime, _tare);
+  // LoadCell.setReverseOutput(); // Uncomment to turn a negative output value to positive
+  unsigned long stabilizingTime = 2000; // Precision right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean tareFlag = true; // Set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingTime, tareFlag);
   if (LoadCell.getTareTimeoutFlag() || LoadCell.getSignalTimeoutFlag()) {
-    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    Serial.println("Timeout, check MCU > HX711 wiring and pin designations");
     while (1);
   }
   else {
-    LoadCell.setCalFactor(1.0); // user set calibration value (float), initial value 1.0 may be used for this sketch
+    LoadCell.setCalFactor(1.0); // User set calibration value (float), initial value 1.0 may be used for this sketch
     Serial.println("Startup is complete");
   }
   while (!LoadCell.update());
-  calibrate(); //start calibration procedure
+  calibrate(); // Start calibration procedure
 }
 
 void loop() {
-  static boolean newDataReady = 0;
-  const int serialPrintInterval = 0; //increase value to slow down serial print activity
+  static boolean newDataReady = false;
+  const int serialPrintInterval = 0; // Increase value to slow down serial print activity
 
-  // check for new data/start next conversion:
+  // Check for new data/start next conversion:
   if (LoadCell.update()) newDataReady = true;
 
-  // get smoothed value from the dataset:
+  // Get smoothed value from the dataset:
   if (newDataReady) {
     if (millis() > t + serialPrintInterval) {
       float i = LoadCell.getData();
-      Serial.print("Load_cell output val: ");
-      Serial.println(i);
-      newDataReady = 0;
+      Serial.print(i);
+      Serial.println(",");
+      newDataReady = false;
       t = millis();
     }
   }
 
-  // receive command from serial terminal
+  // Receive command from serial terminal
   if (Serial.available() > 0) {
     char inByte = Serial.read();
-    if (inByte == 't') LoadCell.tareNoDelay(); //tare
-    else if (inByte == 'r') calibrate(); //calibrate
-    else if (inByte == 'c') changeSavedCalFactor(); //edit calibration value manually
+    if (inByte == 't') LoadCell.tareNoDelay(); // Tare
+    else if (inByte == 'r') calibrate(); // Calibrate
+    else if (inByte == 'c') changeSavedCalFactor(); // Edit calibration value manually
   }
 
-  // check if last tare operation is complete
+  // Check if last tare operation is complete
   if (LoadCell.getTareStatus() == true) {
     Serial.println("Tare complete");
   }
-
 }
 
 void calibrate() {
   Serial.println("***");
   Serial.println("Start calibration:");
-  Serial.println("Place the load cell an a level stable surface.");
+  Serial.println("Place the load cell on a level stable surface.");
   Serial.println("Remove any load applied to the load cell.");
   Serial.println("Send 't' from serial monitor to set the tare offset.");
 
-  boolean _resume = false;
-  while (_resume == false) {
+  boolean resumeFlag = false;
+  while (resumeFlag == false) {
     LoadCell.update();
     if (Serial.available() > 0) {
-      if (Serial.available() > 0) {
-        char inByte = Serial.read();
-        if (inByte == 't') LoadCell.tareNoDelay();
-      }
+      char inByte = Serial.read();
+      if (inByte == 't') LoadCell.tareNoDelay();
     }
     if (LoadCell.getTareStatus() == true) {
       Serial.println("Tare complete");
-      _resume = true;
+      resumeFlag = true;
     }
   }
 
-  Serial.println("Now, place your known mass on the loadcell.");
+  Serial.println("Now, place your known mass on the load cell.");
   Serial.println("Then send the weight of this mass (i.e. 100.0) from serial monitor.");
 
-  float known_mass = 0;
-  _resume = false;
-  while (_resume == false) {
+  float knownMass = 0;
+  resumeFlag = false;
+  while (resumeFlag == false) {
     LoadCell.update();
     if (Serial.available() > 0) {
-      known_mass = Serial.parseFloat();
-      if (known_mass != 0) {
-        Serial.print("Known mass is: ");
-        Serial.println(known_mass);
-        _resume = true;
+      knownMass = Serial.parseFloat();
+      if (knownMass != 0) {
+        Serial.print(knownMass);
+        Serial.println(",");
+        resumeFlag = true;
       }
     }
   }
 
-  LoadCell.refreshDataSet(); //refresh the dataset to be sure that the known mass is measured correct
-  float newCalibrationValue = LoadCell.getNewCalibration(known_mass); //get the new calibration value
+  LoadCell.refreshDataSet(); // Refresh the dataset to ensure that the known mass is measured correctly
+  float newCalibrationValue = LoadCell.getNewCalibration(knownMass); // Get the new calibration value
 
-  Serial.print("New calibration value has been set to: ");
   Serial.print(newCalibrationValue);
-  Serial.println(", use this as calibration value (calFactor) in your project sketch.");
-  Serial.print("Save this value to EEPROM adress ");
-  Serial.print(calVal_eepromAdress);
-  Serial.println("? y/n");
-
-  _resume = false;
-  while (_resume == false) {
-    if (Serial.available() > 0) {
-      char inByte = Serial.read();
-      if (inByte == 'y') {
-#if defined(ESP8266)|| defined(ESP32)
-        EEPROM.begin(512);
-#endif
-        EEPROM.put(calVal_eepromAdress, newCalibrationValue);
-#if defined(ESP8266)|| defined(ESP32)
-        EEPROM.commit();
-#endif
-        EEPROM.get(calVal_eepromAdress, newCalibrationValue);
-        Serial.print("Value ");
-        Serial.print(newCalibrationValue);
-        Serial.print(" saved to EEPROM address: ");
-        Serial.println(calVal_eepromAdress);
-        _resume = true;
-
-      }
-      else if (inByte == 'n') {
-        Serial.println("Value not saved to EEPROM");
-        _resume = true;
-      }
-    }
-  }
-
-  Serial.println("End calibration");
+  Serial.println(",");
   Serial.println("***");
   Serial.println("To re-calibrate, send 'r' from serial monitor.");
-  Serial.println("For manual edit of the calibration value, send 'c' from serial monitor.");
+  Serial.println("For manual editing of the calibration value, send 'c' from serial monitor.");
   Serial.println("***");
 }
 
 void changeSavedCalFactor() {
   float oldCalibrationValue = LoadCell.getCalFactor();
-  boolean _resume = false;
+  boolean resumeFlag = false;
   Serial.println("***");
-  Serial.print("Current value is: ");
-  Serial.println(oldCalibrationValue);
-  Serial.println("Now, send the new value from serial monitor, i.e. 696.0");
-  float newCalibrationValue;
-  while (_resume == false) {
-    if (Serial.available() > 0) {
-      newCalibrationValue = Serial.parseFloat();
-      if (newCalibrationValue != 0) {
-        Serial.print("New calibration value is: ");
-        Serial.println(newCalibrationValue);
-        LoadCell.setCalFactor(newCalibrationValue);
-       
+  Serial.print(oldCalibrationValue);
+  Serial.println(",");
+  Serial.println("Now, send the
